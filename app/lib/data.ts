dc198b5322bc86a297197f40b0a26238743fd040
Ignat S <mail@igonato.com>
2023-11-05 22:43:03 +0300

    Chapter 7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,4 @@
-import { sql } from "@vercel/postgres";
+import { QueryResult, sql } from "@vercel/postgres";
 import {
     CustomerField,
     CustomersTable,
@@ -18,10 +18,10 @@ export async function fetchRevenue() {
         // Artificially delay a reponse for demo purposes.
         // Don't do this in real life :)
 
-        // console.log('Fetching revenue data...');
+        // console.log("Fetching revenue data...");
         // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-        const data = await sql<Revenue>`SELECT * FROM revenue`;
+        const data: QueryResult<Revenue> = await sql`SELECT * FROM revenue`;
 
         // console.log('Data fetch complete after 3 seconds.');
 
@@ -35,11 +35,12 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
     try {
         const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+            SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+            FROM invoices
+            JOIN customers ON invoices.customer_id = customers.id
+            ORDER BY invoices.date DESC
+            LIMIT 5
+        `;
 
         const latestInvoices = data.rows.map((invoice) => ({
             ...invoice,
@@ -60,9 +61,9 @@ export async function fetchCardData() {
         const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
         const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
         const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
+            SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+            SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+            FROM invoices`;
 
         const data = await Promise.all([
             invoiceCountPromise,
